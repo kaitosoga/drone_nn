a4e7e5c15425afc4d07c6b2a1034b582f57713ca
Kaito0733 <riossart@gmail.com>
2026-01-20 17:16:34 +0100

    tested pages

--- frontend/angular-app/src/app/app.component.ts
+++ frontend/angular-app/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  
   template: `
     <h1>Hello Angular</h1>
     <p>2 + 3 = {{ 2 + 3 }}</p>
     <button (click)="increment()">Clicked {{ count }} times</button>
-  `
+  `,
+
+    standalone: true,
+    imports: [RouterOutlet, RouterLink],  // Add these
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  title = 'app'
+
   count = 0;
 
   increment() {
